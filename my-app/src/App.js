05caca119fdc91c30ee4b409f8fe3d5bd680245c
MaksymLytvyn <maxim.lytvyn@gmail.com>
2023-01-10 21:34:47 +0100

    42

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home';
+import Logowanie from './components/Logowanie';
+import Rejestracja from './components/Rejestracja';
+import KrokPoKroku from './components/KrokPoKroku';
+import Onas from './components/Onas';
+import Fundacja from './components/Fundacja';
+import Kontakt from './components/Kontakt';
+import Wylogowano from './components/Wylogowano';
+
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <Route exact path='/' component={Home} />
+          <Route path='/logowanie' component={Logowanie} />
+          <Route path='/rejestracja' component={Rejestracja} />
+          <Route path='/start' component={Home} />
+          <Route path='/krok-po-kroku' component={KrokPoKroku} />
+          <Route path='/o-nas' component={Onas} />
+          <Route path='/fundacja-i-organizacje' component={Fundacja} />
+          <Route path='/kontakt' component={Kontakt} />
+          <Route path='/wylogowano' component={Wylogowano} />
+        </Router>
+    )
+  }
 }
 
 export default App;
+
